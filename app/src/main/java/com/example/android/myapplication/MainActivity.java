151d41548b4d156787ea3f2b3d526764d3d6e051
nesscollege2016 <nesscollege2016@gmail.com>
2016-05-02 09:23:45 +0300

    seekbar demo

--- app/src/main/java/com/example/android/myapplication/MainActivity.java
+++ app/src/main/java/com/example/android/myapplication/MainActivity.java
@@ -4,27 +4,43 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.ProgressBar;
+import android.widget.SeekBar;
 import android.widget.TextView;
 
 import java.util.Random;
 
 public class MainActivity extends AppCompatActivity {
-
-    TextView tvHello;
-    Button btn;
+    TextView tvSize;
+    SeekBar sbSize;
+    ProgressBar pbSize;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        btn = (Button) findViewById(R.id.btn);
-        tvHello = (TextView) findViewById(R.id.myText);
+        tvSize = (TextView) findViewById(R.id.tvSize);
+        sbSize = (SeekBar) findViewById(R.id.sbSize);
+        pbSize = (ProgressBar) findViewById(R.id.pbSize);
+
+        sbSize.setOnSeekBarChangeListener(new SeekBar.OnSeekBarChangeListener() {
+            public void onProgressChanged(SeekBar seekBar, int progress, boolean fromUser) {
+                progress += 10;
+                tvSize.setText("" + progress);
+                tvSize.setTextSize(progress);
+                pbSize.setProgress(progress * 8);
+
+            }
+            public void onStartTrackingTouch(SeekBar seekBar) {
+
+            }
+            public void onStopTrackingTouch(SeekBar seekBar) {
+
+            }
+        });
     }
 
-    public void btnClicked(View view) {
-        Random r = new Random();
 
-        tvHello.setText(r.nextInt(11) + "");
-    }
+
 }
---
+++ app/src/main/java/com/example/android/myapplication/MyClickListener.java
@@ -0,0 +1,14 @@
+package com.example.android.myapplication;
+
+import android.content.DialogInterface;
+import android.view.View;
+
+/**
+ * Created by android on 02/05/2016.
+ */
+public class MyClickListener implements View.OnClickListener {
+    @Override
+    public void onClick(View v) {
+        System.out.println("Hello");
+    }
+}
