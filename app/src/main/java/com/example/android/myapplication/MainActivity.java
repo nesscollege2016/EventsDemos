7b4bb19178109533f54a625aaff2eb7942d73df6
nesscollege2016 <nesscollege2016@gmail.com>
2016-05-02 11:02:20 +0300

    colorPickers with edit text working

--- app/src/main/java/com/example/android/myapplication/MainActivity.java
+++ app/src/main/java/com/example/android/myapplication/MainActivity.java
@@ -3,6 +3,8 @@ package com.example.android.myapplication;
 import android.graphics.Color;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.text.Editable;
+import android.text.TextWatcher;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
@@ -12,7 +14,7 @@ import android.widget.TextView;
 
 import java.util.Random;
 
-public class MainActivity extends AppCompatActivity implements SeekBar.OnSeekBarChangeListener {
+public class MainActivity extends AppCompatActivity implements SeekBar.OnSeekBarChangeListener, TextWatcher {
 
     SeekBar sbRed, sbGreen, sbBlue;
     EditText etRed, etGreen, etBlue, etTotal;
@@ -32,6 +34,10 @@ public class MainActivity extends AppCompatActivity implements SeekBar.OnSeekBar
         sbRed.setOnSeekBarChangeListener(this);
         sbBlue.setOnSeekBarChangeListener(this);
         sbGreen.setOnSeekBarChangeListener(this);
+
+        etRed.addTextChangedListener(this);
+        etGreen.addTextChangedListener(this);
+        etBlue.addTextChangedListener(this);
     }
 
     private void initLayout() {
@@ -45,8 +51,24 @@ public class MainActivity extends AppCompatActivity implements SeekBar.OnSeekBar
         etTotal = (EditText) findViewById(R.id.etTotal);
     }
 
+    boolean fromCode = false;
     public void onProgressChanged(SeekBar seekBar, int progress, boolean fromUser) {
-        etTotal.setBackgroundColor(Color.rgb(sbRed.getProgress(), sbGreen.getProgress(), sbBlue.getProgress()));
+
+        int color = Color.rgb(sbRed.getProgress(), sbGreen.getProgress(), sbBlue.getProgress());
+        String totalColor = "#" + Integer.toHexString(color).toUpperCase().substring(2);
+        etTotal.setText(totalColor);
+        etTotal.setBackgroundColor(color);
+
+        if (fromUser) {
+            fromCode = true;
+            etRed.setText(sbRed.getProgress() + "");
+            etGreen.setText(sbGreen.getProgress() + "");
+            etBlue.setText(sbBlue.getProgress() + "");
+            fromCode = false;
+
+        }
+
+
     }
 
     public void onStartTrackingTouch(SeekBar seekBar) {
@@ -54,5 +76,33 @@ public class MainActivity extends AppCompatActivity implements SeekBar.OnSeekBar
     }
     public void onStopTrackingTouch(SeekBar seekBar) {
 
+    }
+
+
+    public void beforeTextChanged(CharSequence s, int start, int count, int after) {
+
+    }
+    public void onTextChanged(CharSequence s, int start, int before, int count) {
+
+    }
+
+    @Override
+    public void afterTextChanged(Editable s) {
+        try {
+            Integer redProgress = Integer.valueOf(etRed.getText().toString());
+            Integer greenProgress = Integer.valueOf(etGreen.getText().toString());
+            Integer blueProgress = Integer.valueOf(etBlue.getText().toString());
+
+
+            if (!fromCode) {
+                sbRed.setProgress(redProgress);
+                sbGreen.setProgress(greenProgress);
+                sbBlue.setProgress(blueProgress);
+            }
+
+        }
+        catch (Exception e){
+            e.printStackTrace();
+        }
     }
 }
