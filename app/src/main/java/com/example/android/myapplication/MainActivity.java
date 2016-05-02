3b9fd740c5ed948ab929ace4e0c942465df74d2c
nesscollege2016 <nesscollege2016@gmail.com>
2016-05-02 09:38:57 +0300

    colorPickers starter layout

--- app/src/main/java/com/example/android/myapplication/MainActivity.java
+++ app/src/main/java/com/example/android/myapplication/MainActivity.java
@@ -11,36 +11,10 @@ import android.widget.TextView;
 import java.util.Random;
 
 public class MainActivity extends AppCompatActivity {
-    TextView tvSize;
-    SeekBar sbSize;
-    ProgressBar pbSize;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-
-        tvSize = (TextView) findViewById(R.id.tvSize);
-        sbSize = (SeekBar) findViewById(R.id.sbSize);
-        pbSize = (ProgressBar) findViewById(R.id.pbSize);
-
-        sbSize.setOnSeekBarChangeListener(new SeekBar.OnSeekBarChangeListener() {
-            public void onProgressChanged(SeekBar seekBar, int progress, boolean fromUser) {
-                progress += 10;
-                tvSize.setText("" + progress);
-                tvSize.setTextSize(progress);
-                pbSize.setProgress(progress * 8);
-
-            }
-            public void onStartTrackingTouch(SeekBar seekBar) {
-
-            }
-            public void onStopTrackingTouch(SeekBar seekBar) {
-
-            }
-        });
     }
-
-
-
 }
